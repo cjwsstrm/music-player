976a09a3b09ba3cede82e98cc1291b463e7221a8
CJ Wesstrom <cjwesstrom@gmail.com>
2018-03-06 11:06:24 -0800

    added basic logged in example displaying currentlyplaying song

--- react-music-player/src/App.js
+++ react-music-player/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import Player from './components/player';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
 
 
 
@@ -9,13 +12,21 @@ class App extends Component {
   constructor() {
     super();
     const params = this.getHashParams();
-    console.log(params);
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    };
+    // console.log(params);
   }
   getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
       q = window.location.hash.substring(1);
-    e = r.exec(q)
+    e = r.exec(q);
     while (e) {
       hashParams[e[1]] = decodeURIComponent(e[2]);
       e = r.exec(q);
@@ -23,12 +34,35 @@ class App extends Component {
     return hashParams;
   }
 
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url
+          }
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <div className='App'>
+        <div className='AppSpot'>
           <a href='http://localhost:8888'> Login to Spotify </a>
+          <div>
+            Now Playing: {this.state.nowPlaying.name}
+          </div>
+          <div>
+            <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+          </div>
+          {this.state.loggedIn &&
+            <button onClick={() => this.getNowPlaying()}>
+              Check Now Playing
+            </button>
+          }
         </div>
         <Player />
       </div>
