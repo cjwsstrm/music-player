a8eb37d67d290686746833df7add1cf9af611b9d
CJ Wesstrom <cjwesstrom@gmail.com>
2018-03-09 13:13:29 -0800

    added wave visualisation (bars commented out for now)

--- react-music-player/src/components/visualiser.js
+++ react-music-player/src/components/visualiser.js
@@ -17,8 +17,8 @@ export default class Visualiser extends Component {
       };
       // Find canvas and set context
       canvas = document.getElementById('canvas-main-visualisation');
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+      canvas.width = document.querySelector('.visualisation').clientWidth;
+      canvas.height = document.querySelector('.visualisation').clientHeight;
       ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -28,45 +28,73 @@ export default class Visualiser extends Component {
       analyser.minDecibels = -90;
       analyser.maxDecibels = -10;
       analyser.smoothingTimeConstant = .9;
-      analyser.fftSize = 128;
-      
+      analyser.fftSize = 2048;
+
       var source = audioContext.createMediaElementSource(audio);
       source.connect(analyser);
       analyser.connect(audioContext.destination);
-      
+
       var bufferLength = analyser.frequencyBinCount;
       var dataArray = new Uint8Array(bufferLength);
-      
+
       function draw() {
         requestAnimationFrame(draw);
-        
-        analyser.getByteFrequencyData(dataArray);
-        
+
+        // analyser.getByteFrequencyData(dataArray);
+        analyser.getByteTimeDomainData(dataArray);
+
+
         ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
+
         // Bar visualisation
-        var barWidth = (canvas.width / bufferLength) * 1.5;
-        var barHeight;
+        // var barWidth = (canvas.width / bufferLength) * 1.5;
+        // var barHeight;
+        // var x = 0;
+
+        // for (var i = 0; i < bufferLength; i++) {
+        //   barHeight = dataArray[i];
+        //   var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+        //   gradient.addColorStop(0.5, 'orange');
+        //   gradient.addColorStop(0.75, 'yellow');
+        //   gradient.addColorStop(1, 'green');
+        //   ctx.fillStyle = gradient;
+        //   ctx.fillRect(x, canvas.height - barHeight, barWidth, (barHeight));
+
+        //   x += barWidth + 1;
+
+        // Wave visualisation
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'white';
+
+        ctx.beginPath();
+
+        var sliceWidth = canvas.width / bufferLength;
         var x = 0;
 
         for (var i = 0; i < bufferLength; i++) {
-          barHeight = dataArray[i];
-          var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-          gradient.addColorStop(0.5, 'orange');
-          gradient.addColorStop(0.75, 'yellow');
-          gradient.addColorStop(1, 'green');
-          ctx.fillStyle = gradient;
-          ctx.fillRect(x, canvas.height - barHeight * 3.5, barWidth, (barHeight * 3.5));
 
-          x += barWidth + 1.2;
+          var v = dataArray[i] / 128.0;
+          var y = v * canvas.height / 2;
 
+          if (i === 0) {
+            ctx.moveTo(x, y);
+          } else {
+            ctx.lineTo(x, y);
+          }
+
+          x += sliceWidth + 1;
         }
+        ctx.lineTo(canvas.width, canvas.height / 2);
+        ctx.stroke();
+
       }
       draw();
     };
 
   }
+
+  
 
   render() {
     return (
