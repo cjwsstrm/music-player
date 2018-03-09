ed8eef840f7727e8aca9a6c504df3ce233171ef8
CJ Wesstrom <cjwesstrom@gmail.com>
2018-03-09 10:33:08 -0800

    updated bar visualisation to now color bars on a gradient

--- react-music-player/src/components/visualiser.js
+++ react-music-player/src/components/visualiser.js
@@ -3,44 +3,64 @@ import React, { Component } from 'react';
 export default class Visualiser extends Component {
 
   componentDidMount() {
-    // Initial Bar Visualisation
     var analyser, canvas, ctx;
-    // random = Math.random, circles = [];
 
     window.onload = function () {
       var file = document.getElementById("thefile");
       var audio = document.getElementById("audio");
-      
+
       file.onchange = function () {
         var files = this.files;
         audio.src = URL.createObjectURL(files[0]);
         audio.load();
-        // audio.style.width = 400 + 'px';
 
       };
-      var audioContext = new window.AudioContext();
-      analyser = audioContext.createAnalyser();
-      // analyser.fftsize = 256;
-      var source = audioContext.createMediaElementSource(audio);
-
+      // Find canvas and set context
       canvas = document.getElementById('canvas-main-visualisation');
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+      // Set audioContext and properties
+      var audioContext = new window.AudioContext();
+      analyser = audioContext.createAnalyser();
+      analyser.minDecibels = -90;
+      analyser.maxDecibels = -10;
+      analyser.smoothingTimeConstant = .9;
+      analyser.fftSize = 128;
+      
+      var source = audioContext.createMediaElementSource(audio);
       source.connect(analyser);
       analyser.connect(audioContext.destination);
-
+      
+      var bufferLength = analyser.frequencyBinCount;
+      var dataArray = new Uint8Array(bufferLength);
+      
       function draw() {
-        var freqByteData = new Uint8Array(analyser.frequencyBinCount);
         requestAnimationFrame(draw);
-        analyser.getByteFrequencyData(freqByteData);
+        
+        analyser.getByteFrequencyData(dataArray);
+        
+        ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // Bar visualisation
+        var barWidth = (canvas.width / bufferLength) * 1.5;
+        var barHeight;
+        var x = 0;
 
-        for (var i = 1; i < freqByteData.length; i += 10) {
-          ctx.fillStyle = `orange`;
-          ctx.fillRect(i, canvas.height - freqByteData[i] * 3, 10, canvas.height);
-          ctx.strokeRect(i, canvas.height - freqByteData[i] * 3, 10, canvas.height);
+        for (var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+          var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+          gradient.addColorStop(0.5, 'orange');
+          gradient.addColorStop(0.75, 'yellow');
+          gradient.addColorStop(1, 'green');
+          ctx.fillStyle = gradient;
+          ctx.fillRect(x, canvas.height - barHeight * 3, barWidth, (barHeight * 3));
+
+          x += barWidth + 1.35;
+
         }
       }
       draw();
