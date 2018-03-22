443dd8c6375629a78dcfdd2ea302724ff791c187
CJ Wesstrom <cjwesstrom@gmail.com>
2018-03-22 11:28:46 -0700

    changed background color to white. Started implementation of onclick changing visualisation

--- react-music-player/src/components/visualiser.js
+++ react-music-player/src/components/visualiser.js
@@ -1,74 +1,88 @@
 import React, { Component } from 'react';
 
 export default class Visualiser extends Component {
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
+    this.state = {
+      currentVisual: 'bars'
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.visualise = this.visualise.bind(this);
+    this.drawVisual;
+  }
+
+  visualise() {
+    console.log('test');
     var analyser, canvas, ctx;
 
-    window.onload = function () {
-      var file = document.getElementById("thefile");
-      var audio = document.getElementById("audio");
+    // window.onload = function () {
+    var file = document.getElementById("thefile");
+    var audio = document.getElementById("audio");
 
-      file.onchange = function () {
-        var files = this.files;
-        audio.src = URL.createObjectURL(files[0]);
-        audio.load();
+    file.onchange = function () {
+      var files = this.files;
+      audio.src = URL.createObjectURL(files[0]);
+      audio.load();
 
-      };
-      // Find canvas and set context
-      canvas = document.getElementById('canvas-main-visualisation');
-      canvas.width = document.querySelector('.visualisation').clientWidth;
-      canvas.height = document.querySelector('.visualisation').clientHeight;
-      ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+    // Find canvas and set context
+    canvas = document.getElementById('canvas-main-visualisation');
+    canvas.width = document.querySelector('.visualisation').clientWidth;
+    canvas.height = document.querySelector('.visualisation').clientHeight;
+    ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Set audioContext and properties
-      var audioContext = new window.AudioContext();
-      analyser = audioContext.createAnalyser();
-      analyser.minDecibels = -90;
-      analyser.maxDecibels = -10;
-      analyser.smoothingTimeConstant = .9;
-      analyser.fftSize = 2048;
+    // Set audioContext and properties
+    var audioContext = new window.AudioContext();
+    analyser = audioContext.createAnalyser();
+    analyser.minDecibels = -90;
+    analyser.maxDecibels = -10;
+    analyser.smoothingTimeConstant = .9;
 
-      var source = audioContext.createMediaElementSource(audio);
-      source.connect(analyser);
-      analyser.connect(audioContext.destination);
+    var source = audioContext.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
 
+    // Bar visualisation
+    if (this.state.currentVisual === 'bars') {
+      analyser.fftSize = 128;
       var bufferLength = analyser.frequencyBinCount;
       var dataArray = new Uint8Array(bufferLength);
+      var drawBars = () => {
 
+        this.drawVisual = requestAnimationFrame(drawBars);
 
-      // Bar visualisation
-      // function draw() {
-      //   requestAnimationFrame(draw);
+        analyser.getByteFrequencyData(dataArray);
 
-      //   analyser.getByteFrequencyData(dataArray);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var barWidth = (canvas.width / bufferLength);
+        var barHeight;
+        var x = 0;
 
-      //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-      //   var barWidth = (canvas.width / bufferLength);
-      //   var barHeight;
-      //   var x = 0;
+        for (var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+          var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+          gradient.addColorStop(0.5, 'orange');
+          gradient.addColorStop(0.75, 'yellow');
+          gradient.addColorStop(1, 'green');
+          ctx.fillStyle = gradient;
+          ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
 
-      //   for (var i = 0; i < bufferLength; i++) {
-      //     barHeight = dataArray[i];
-      //     var gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-      //     gradient.addColorStop(0.5, 'orange');
-      //     gradient.addColorStop(0.75, 'yellow');
-      //     gradient.addColorStop(1, 'green');
-      //     ctx.fillStyle = gradient;
-      //     ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+          x += barWidth + 1;
+        }
+      };
+      drawBars();
+    }
 
-      //     x += barWidth + 1;
-      //   }
-      // }
-      // draw();
+    // Wave visualisation
 
-
-      // Wave visualisation
-      
-      function draw() {
-
-        requestAnimationFrame(draw);
+    if (this.state.currentVisual === 'wave') {
+      analyser.fftSize = 2048;
+      bufferLength = analyser.frequencyBinCount;
+      dataArray = new Uint8Array(bufferLength);
+      var drawWave = () => {
+        this.drawVisual = requestAnimationFrame(drawWave);
 
         analyser.getByteTimeDomainData(dataArray);
 
@@ -96,18 +110,33 @@ export default class Visualiser extends Component {
         }
         ctx.lineTo(canvas.width, canvas.height / 2);
         ctx.stroke();
-      }
-
-      draw();
-    };
-
+      };
+      drawWave();
+    }
   }
 
-
+  handleClick() {
+    this.visualise();
+    if (this.state.currentVisual === 'bars') {
+      this.setState({
+        currentVisual: 'wave'
+      });
+      window.cancelAnimationFrame(this.drawVisual);
+      this.visualize;
+    }
+    if (this.state.currentVisual === 'wave') {
+      this.setState({
+        currentVisual: 'bars'
+      });
+    }
+    window.cancelAnimationFrame(this.drawVisual);
+    this.visualize;
+    console.log('currentState: ' + this.state.currentVisual);
+  }
 
   render() {
     return (
-      <div className="visualisation" id="visualisation">
+      <div className="visualisation" id="visualisation" onClick={this.handleClick}>
         <canvas id="canvas-main-visualisation"> </canvas>
       </div>
     );
